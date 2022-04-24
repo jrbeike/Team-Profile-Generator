ed757fb89509469b7616e7099603210abd4dca53
jrbeike <jrbeike@gmail.com>
2022-04-24 19:53:10 -0400

    update app.js

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const inquirer = require("inquirer");
-const genreatePage = require('./src/page-template')
+const genreatePage = require('./src/page-template.js');
+const { writeFile, copyFile } = require ('./utils/generate-site');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+
