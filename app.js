f56d604f686625f1c34bcbc69c824693acd81c5a
jrbeike <jrbeike@gmail.com>
2022-04-24 21:04:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const inquirer = require("inquirer");
 const genreatePage = require('./src/page-template.js');
-const { writeFile, copyFile } = require ('./utils/generate-site');
+const { writeFile, copyFile } = require('./utils/generate-site');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
-
+function initApp() {
+    startHtml();
+    addMemeber();
+}
+const promtUser = () => {
+    return inquirer.prompt([
+        {
+            message: "Enter Team Memebers Name",
+            name: "name",
+        },
+        {
+            
+        }
+    ])
+}
